62a078b06a50518b1421eaf01f146b5416151e6a
Jhonatan Riano <jriano1946@gmail.com>
2022-12-05 00:24:11 -0500

    metodo numero mayor

---
+++ DigitalConsole/DigitalConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[5] { 5, 25, 3, 12, 15 };
+            int numeroMayor = 0;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                numeroMayor = myArray[i] > numeroMayor ? myArray[i] : numeroMayor;
+            }
+            Console.WriteLine(numeroMayor);
+        }
+    }
+}
