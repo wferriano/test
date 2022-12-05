41335b3ac0c52037a7d94f81980e86d184d45ad6
Jhonatan Riano <jriano1946@gmail.com>
2022-12-05 10:08:28 -0500

    Suma

---
+++ Suma/Suma/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Suma
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[6] { 1, 8, 6, 7, 2, 5 };
+            for (int i = 0; i < myArray.Length - 1; i++)
+            {
+                for (int j = 1; j < myArray.Length; j++)
+                {
+                    if (myArray[i] + myArray[j] == 10)
+                    {
+                        Console.WriteLine(myArray[i] + " " + myArray[j]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
